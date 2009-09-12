d5ceb6d78b6a5322bfbfb89a8ba952c2abed2c5f
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-09-12 21:44:47 +0800

    ENH: first draft

--- Prototype1/Prototype1.cxx
+++ Prototype1/Prototype1.cxx
@@ -1,7 +1,23 @@
 #include <cstdlib>
 
+#include "otbImage.h"
+#include "otbImageFileReader.h"
+#include "itkBinaryThresholdImageFilter.h"
+
 int main(int argc, char* argv[])
 {
+
+  typedef otb::Image<double, 2> ImageType;
+  typedef otb::ImageFileReader<ImageType> ReaderType;
+  typedef itk::BinaryThresholdImageFilter<ImageType, ImageType> ThresholdType;
+  typedef itk::ProcessObject ProcessType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  ProcessType::Pointer process1 = reader.GetPointer();
+
+  ProcessType::Pointer process2 = (ThresholdType::New()).GetPointer();
+
+  process2->GetInputs()[0] = process1->GetOutputs()[0];
 
   return EXIT_SUCCESS;
 }
