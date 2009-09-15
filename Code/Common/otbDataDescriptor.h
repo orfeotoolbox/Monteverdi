08543a07bf5101757311577ba779d8e772e7d45e
Julien Michel <julien.michel@orfeo-toolbox.org>
2009-09-15 10:51:36 +0200

    ENH: Adding class DataDescriptor

---
+++ Code/Common/otbDataDescriptor.h
@@ -0,0 +1,71 @@
+/*=========================================================================
+
+Program:   ORFEO Toolbox
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+
+Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+See OTBCopyright.txt for details.
+
+
+This software is distributed WITHOUT ANY WARRANTY; without even
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __otbDataDescriptor_h
+#define __otbDataDescriptor_h
+
+#include <string>
+#include <iostream>
+
+namespace otb
+{
+
+/** \class DataDescriptor
+ *  \brief This small class describes the input/outputs of a module.
+ *
+ *  \sa Module
+ */
+
+class DataDescriptor
+{
+public:
+  /** Constructors */
+  DataDescriptor();
+  DataDescriptor(const std::string & type, const std::string & key, const std::string & description);
+
+  /** Destructor */
+  virtual ~DataDescriptor();
+
+  /** Set the data object mandatory types */
+  void Set(const std::string & type, const std::string & key, const std::string & description);
+
+  /** Get the DataType */
+  const std::string & GetDataType() const;
+
+  /** Get the DataKey */
+  const std::string & GetDataKey() const;
+
+  /** Get the DataDescription */
+  const std::string & GetDataDescription() const;
+
+private:
+  /** The data type */
+  std::string m_DataType;
+
+  /** The key to identify input/output of a module */
+  std::string m_DataKey;
+
+  /** A description of the input/output */
+  std::string m_DataDescription;
+};
+
+/** Overloading the << operator */
+std::ostream & operator<<(std::ostream & ostr, const DataDescriptor & descriptor);
+
+} // End namespace otb
+
+#endif
---
+++ Testing/Common/otbDataDescriptorTest.cxx
@@ -0,0 +1,40 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "otbDataDescriptor.h"
+
+
+int main(int argc, char * argv[])
+{
+  // Default constructor and call to the set method
+  otb::DataDescriptor desc1;
+  desc1.Set("Floating_Point_Image","InputImage","This is the input image");
+
+  // Access parameters
+  std::cout<<"Desc1: DataType = "<<desc1.GetDataType()<<", DataKey = "<<desc1.GetDataKey()<<", DataDescripion = "<<desc1.GetDataDescription()<<std::endl;
+
+  // Constructor with parameters
+  otb::DataDescriptor desc2("Floating_Point_Image","InputImage","This is the input image");
+  
+  // Access parameters
+  std::cout<<"Desc2: DataType = "<<desc2.GetDataType()<<", DataKey = "<<desc1.GetDataKey()<<", DataDescripion = "<<desc2.GetDataDescription()<<std::endl;
+ 
+  // Testing the << operator
+  std::cout<<"Testing << operator: "<<desc2<<std::endl;
+
+  return EXIT_SUCCESS;
+}
