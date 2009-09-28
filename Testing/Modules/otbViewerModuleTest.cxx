885dc3813f0d3c5539ca536d93dd4775528709b8
Otmane Lahlou <otmane.lahlou@c-s.fr>
2009-09-28 08:51:57 +0200

    ENH : replace Fl::run with Fl::check in tests

--- Testing/Modules/otbViewerModuleTest.cxx
+++ Testing/Modules/otbViewerModuleTest.cxx
@@ -15,60 +15,6 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
-
-// #include "otbViewerModule.h"
-
-// #include "otbImage.h"
-// #include "otbImageFileReader.h"
-// #include "otbImageFileWriter.h"
-
-// #include "otbVectorData.h"
-// #include "otbVectorDataFileReader.h"
-
-// int main(int argc, char* argv[])
-// {
-//   otb::ViewerModule::Pointer specificModule = otb::ViewerModule::New();
-//   otb::Module::Pointer module = specificModule.GetPointer();
-  
-//   std::cout<<"Module: "<<module<<std::endl;
-
-//   // Put in the tests
-//   const char * infname = argv[1];
-//   const char * vfname  = argv[2];
-  
-//   typedef otb::VectorImage<double,2>          ImageType;
-//   typedef otb::ImageFileReader<ImageType>     ReaderType;
-//   typedef otb::ImageFileWriter<ImageType>     WriterType;
-
-//   typedef otb::VectorData<double>                    VectorDataType;
-//   typedef otb::VectorDataFileReader<VectorDataType>  VReaderType;
-  
-//   // Image reader
-//   ReaderType::Pointer reader = ReaderType::New();
-//   reader->SetFileName(infname);
-//   reader->GenerateOutputInformation();
-  
-//   otb::DataObjectWrapper wrapperIn("Floating_Point_VectorImage",reader->GetOutput());
-//   std::cout<<"Input wrapper: "<<wrapperIn<<std::endl;
-  
-//   // VectorData Reader 
-//   VReaderType::Pointer vreader = VReaderType::New();
-//   vreader->SetFileName(vfname);
-//   vreader->GenerateOutputInformation();
-  
-//   otb::DataObjectWrapper wrapperVector("Floating_Point_VectorData",vreader->GetOutput());
-//   std::cout <<"Input VectorData Wrapper " <<wrapperVector << std::endl;
-  
-//   module->AddInputByKey("InputImage",wrapperIn);
-//   module->AddInputByKey("VectorData",wrapperVector);
-//   module->Start();
-//   Fl::check();
-
-//   return 0;
-
-// }
-
-
 #include "otbViewerModule.h"
 
 #include "otbImage.h"
@@ -133,7 +79,7 @@ int main(int argc, char* argv[])
   }
   
   module->Start();
-  Fl::run();
+  Fl::check();
 
   return 0;
 
