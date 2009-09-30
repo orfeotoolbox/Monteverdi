6f2065a5922bd0b9977835b54ca29d2a9cb6430e
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2009-09-30 09:24:28 +0200

    ENH : correct Speckle SGFT

--- Testing/Modules/otbSpeckleFilteringApplication.cxx
+++ Testing/Modules/otbSpeckleFilteringApplication.cxx
@@ -27,19 +27,15 @@
 
 int main(int argc, char* argv[])
 {
+  typedef otb::SpeckleFilteringView       ViewType;
+  typedef otb::SpeckleFilteringController ControllerType;
+  typedef otb::SpeckleFilteringModel      ModelType;
 
-  typedef otb::SpeckleFilteringView              ViewType;
-  typedef otb::SpeckleFilteringController           ControllerType;
-  typedef ControllerType::ModelType                 ModelType;
 
   // Instanciation of pointer
-  ViewType::Pointer          view       = ViewType::New();
-  ControllerType::Pointer    controller = ControllerType::New();
-
-  //Set The model input Image
-  ModelType * model = ModelType::New();
-  std::cout<<"Model: "<<model<<std::endl;
-
+  ViewType::Pointer       view       = ViewType::New();
+  ControllerType::Pointer controller = ControllerType::New();
+  ModelType::Pointer      model      = ModelType::New();
 
   controller->SetModel(model);
   controller->SetView(view);
@@ -47,21 +43,17 @@ int main(int argc, char* argv[])
 
   // Put in the tests
   const char * infname = argv[1];
-  typedef otb::Image<double,2>  ImageType;
-  typedef otb::ImageFileReader<ImageType>     ReaderType;
-  typedef otb::ImageFileWriter<ImageType>     WriterType;
+  typedef ModelType::InputImageType       ImageType;
+  typedef otb::ImageFileReader<ImageType> ReaderType;
+  typedef otb::ImageFileWriter<ImageType> WriterType;
 
 
   //reader
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName(infname);
-  reader->UpdateOutputInformation();
-  std::cout<<"Model: "<<model<<std::endl;
-  std::cout<<"Reader: "<<(ReaderType *)reader<<std::endl;
-  std::cout<<"Output: "<<reader->GetOutput()<<std::endl;
-  std::cout<<"Test."<<std::endl;
+  reader->GenerateOutputInformation();
+
   model->SetInputImage(reader->GetOutput());
-  std::cout<<"Test2."<<std::endl;
 
 
   // Open the GUI
