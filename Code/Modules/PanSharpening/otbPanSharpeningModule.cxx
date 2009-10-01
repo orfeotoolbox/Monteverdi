185519187097c10c44f036f9442c603509477f7d
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-10-01 17:29:37 +0800

    ENH: starting the Pan Sharpening Module from the StarterKit

---
+++ Code/Modules/PanSharpening/otbPanSharpeningModule.cxx
@@ -0,0 +1,144 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "otbPanSharpeningModule.h"
+
+namespace otb
+{
+/** Constructor */
+PanSharpeningModule::PanSharpeningModule()
+{
+  // First, do constructor stuffs
+
+  // Then, describe inputs needed by the module
+
+  // Add a new input
+  this->AddInputDescriptor<FPVImageType>("MyInputImage","This is my input");
+  
+  // Add another supported type for this input
+  this->AddTypeToInputDescriptor<FPImageType>("MyInputImage");
+
+  // Add an optional input
+  this->AddInputDescriptor<VectorType>("MyOptionalInput","This is my optional input",true);
+
+  // add a multiple input (multiple can also be optional)
+  this->AddInputDescriptor<VectorType>("MyMultipleInput","This is my multiple input",false,true);
+}
+
+/** Destructor */
+PanSharpeningModule::~PanSharpeningModule()
+{}
+
+/** PrintSelf method */
+void PanSharpeningModule::PrintSelf(std::ostream& os, itk::Indent indent) const
+{
+  // Call superclass implementation
+  Superclass::PrintSelf(os,indent);
+}
+
+
+/** The custom run command */
+void PanSharpeningModule::Run()
+{
+  // Here is the body of the module. 
+  // When the Run() method is called, necessary inputs have been
+  // passed to the module.
+
+  // First step is to retrieve the inputs
+  
+  // To handle an input with multiple supported type :
+  FPVImageType::Pointer fpvImage = this->GetInputData<FPVImageType>("MyInputImage");
+  FPImageType::Pointer fpImage = this->GetInputData<FPImageType>("MyInputImage");
+  
+  // One of this pointer will be NULL:
+  if(fpvImage.IsNotNull())
+    {
+    // Process the input as an FPVImageType
+    }
+  else if(fpImage.IsNotNull())
+    {
+    // Process the input as an FPImageType
+    }
+  else
+    {
+    // Rerport error
+    }
+
+  // To handle an optional input: 
+
+  if(this->GetNumberOfInputDataByKey("MyOptionalInput") > 0)
+    {
+    VectorType::Pointer optionalVector = this->GetInputData<VectorType>("MyOptionalInput");
+
+    // Again, check for pointer validity
+    if(optionalVector.IsNotNull())
+      {
+      // Process optional input
+      }
+    else
+      {
+      // Report error
+      }
+    }
+  
+  // To handle a multiple input:
+
+  for(unsigned int inputIdx = 0; inputIdx < this->GetNumberOfInputDataByKey("MyMultipleInput");++inputIdx)
+    {
+    VectorType::Pointer nthMultipleVector = this->GetInputData<VectorType>("MyMultipleInput",inputIdx);
+    
+    if(nthMultipleVector.IsNotNull())
+      {
+      // Process the nth mutliple input
+      }
+    else
+      {
+      // Report error
+      }
+    }
+  
+  // Once all inputs have been properly retrieved, do what the module
+  // should do : show a gui, start an MVC model, trigger processing ...
+  
+
+  // Then, when the module did actually produce some outputs, declare
+  // them.
+
+  // First, clear any previous output
+  this->ClearOutputDescriptors();
+
+  // Add an output (single version)
+  FPImageType::Pointer myBrandNewImageOutput = FPImageType::New();
+  this->AddOutputDescriptor(myBrandNewImageOutput,"MyImageOutput","This is my image output");
+
+  // Add an output (multiple version)
+  
+  FPPointSetType::Pointer pointSetOutput1 = FPPointSetType::New();
+  this->AddOutputDescriptor(pointSetOutput1,"MyPointSetOutput", "These are my pointset outputs");
+  
+  // Add addional data to the same output
+  for(unsigned int i = 0; i<9;++i)
+    {
+    FPPointSetType::Pointer pointSetOutputN = FPPointSetType::New();
+    this->AddDataToOutputDescriptor(pointSetOutputN,"MyPointSetOutput");
+    }
+
+  // Last, when all outputs where declared, notify listeners
+  this->NotifyOutputsChange();
+}
+} // End namespace otb
+
---
+++ Code/Modules/PanSharpening/otbPanSharpeningModule.h
@@ -0,0 +1,82 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __otbPanSharpeningModule_h
+#define __otbPanSharpeningModule_h
+
+// include the base class
+#include "otbModule.h"
+
+// include the OTB/ITK elements
+#include "otbVectorImage.h"
+#include "otbImage.h"
+#include "itkPointSet.h"
+
+namespace otb
+{
+/** \class 
+ *  \brief 
+ * 
+ * Description of the module.
+ *
+ */
+
+class ITK_EXPORT PanSharpeningModule
+  : public Module
+{
+public:
+  /** Standard class typedefs */
+  typedef PanSharpeningModule                 Self;
+  typedef Module                        Superclass;
+  typedef itk::SmartPointer<Self>       Pointer;
+  typedef itk::SmartPointer<const Self> ConstPointer;
+
+  /** New macro */
+  itkNewMacro(Self);
+
+  /** Type macro */
+  itkTypeMacro(PanSharpeningModule,Module);
+
+  /** Data typedefs */
+  /// Dataset
+  typedef VectorImage<double,2>         FPVImageType;
+  typedef Image<double,2>               FPImageType;
+  typedef VectorData<double>            VectorType;
+  typedef itk::PointSet<double,2>       FPPointSetType;
+
+protected:
+  /** Constructor */
+  PanSharpeningModule();
+
+  /** Destructor */
+  virtual ~PanSharpeningModule();
+
+  /** PrintSelf method */
+  virtual void PrintSelf(std::ostream& os, itk::Indent indent) const;
+
+  /** The custom run command */
+  virtual void Run();
+
+private:
+  PanSharpeningModule(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+};
+
+
+} // End namespace otb
+
+#endif
