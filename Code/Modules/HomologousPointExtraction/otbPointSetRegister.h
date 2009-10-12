e038c087f075fcef80bac82cfb9ed758e3ec0139
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2009-10-12 15:31:49 +0200

    ENH :add missing files

---
+++ Code/Modules/HomologousPointExtraction/otbPointSetRegister.h
@@ -0,0 +1,125 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __otbPointSetRegister_h
+#define __otbPointSetRegister_h
+
+
+#include "itkProcessObject.h"
+
+// Estimation
+#include "itkEuclideanDistancePointMetric.h"
+#include "itkLevenbergMarquardtOptimizer.h"
+#include "itkPointSet.h"
+#include "itkPointSetToPointSetRegistrationMethod.h"
+// Transformation
+#include "otbTransformEnumType.h"
+#include "itkTransformBase.h"
+#include "itkAffineTransform.h"
+#include "itkTranslationTransform.h"
+
+
+namespace otb {
+
+/** \class PointSetRegister
+ *
+ * \brief Class to compute a transformation between 2 point sets.
+ *
+ */
+template<class TTransform>
+class ITK_EXPORT PointSetRegister : public itk::ProcessObject
+{
+
+public:
+  /** Standard class typedefs */
+  typedef PointSetRegister          Self;
+  typedef itk::ProcessObject            Superclass;
+  typedef itk::SmartPointer<Self>       Pointer;
+  typedef itk::SmartPointer<const Self> ConstPointer;
+
+  /** Standard type macro */
+  itkTypeMacro(HPointSetRegister, itk::ProcessObject);
+
+  /** New macro */
+  itkNewMacro(Self);
+
+  typedef itk::ProcessObject             ProcessObjectType;
+
+  /** Point Set */
+  typedef itk::PointSet< float, 2 > PointSetType;
+  typedef PointSetType::Pointer     PointSetPointerType;
+  typedef PointSetType::PointType   PointType;
+
+
+  /** Transformation type */
+  typedef TTransform                                   TransformationType;
+  typedef typename TransformationType::Pointer         TransformationPointerType;
+  typedef typename TransformationType::ParametersType  ParametersType;
+
+  /** Transform estimation typedefs */
+  typedef itk::EuclideanDistancePointMetric< PointSetType, PointSetType>       MetricType;
+  typedef MetricType::TransformType                                            TransformType;
+  typedef TransformType::Pointer                                               TransformPointerType;
+  typedef TransformType::JacobianType                                          JacobianType;
+  typedef itk::LevenbergMarquardtOptimizer                                     OptimizerType;
+  typedef itk::PointSetToPointSetRegistrationMethod<PointSetType,PointSetType> RegistrationType;
+
+
+  itkSetObjectMacro(FixPointSet,PointSetType );
+  itkGetObjectMacro(FixPointSet,PointSetType );
+
+  itkSetObjectMacro(MovingPointSet, PointSetType);
+  itkGetObjectMacro(MovingPointSet, PointSetType);
+  
+  itkSetObjectMacro(Transform, TransformationType);
+  itkGetObjectMacro(Transform, TransformationType); 
+  
+  itkGetMacro(TransformParameters, ParametersType); 
+
+  /** Compute transformation */
+  virtual void ComputeTransform();
+ 
+protected:
+  /** Constructor */
+  PointSetRegister();
+  /** Destructor */
+  ~PointSetRegister(){};
+  
+  
+private:
+  PointSetRegister(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  /** Fix point set. */
+  PointSetPointerType m_FixPointSet;
+  /** Moving point set. */
+  PointSetPointerType m_MovingPointSet;  
+  /** Transform base */
+  TransformationPointerType m_Transform;
+  /** Store transformation parameters*/
+  ParametersType m_TransformParameters;
+
+};
+
+}//end namespace otb
+
+#ifndef OTB_MANUAL_INSTANTIATION
+#include "otbPointSetRegister.txx"
+#endif
+
+#endif
---
+++ Code/Modules/HomologousPointExtraction/otbPointSetRegister.txx
@@ -0,0 +1,95 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __otbPointSetRegister_txx
+#define __otbPointSetRegister_txx
+
+#include "otbPointSetRegister.h"
+
+
+
+namespace otb
+{
+template <class TTransform>
+PointSetRegister<TTransform>
+::PointSetRegister() : m_FixPointSet(), m_MovingPointSet(), m_Transform(), m_TransformParameters()
+{
+  m_FixPointSet = PointSetType::New();
+  m_MovingPointSet = PointSetType::New();
+  m_Transform = TransformationType::New();
+}
+
+
+template <class TTransform>
+void
+PointSetRegister<TTransform>
+::ComputeTransform()
+{
+  if(m_FixPointSet->GetNumberOfPoints() == 0 || m_MovingPointSet->GetNumberOfPoints() == 0)
+    {
+      itkExceptionMacro(<<"Input set is empty");
+    }
+  if(m_FixPointSet->GetNumberOfPoints() != m_MovingPointSet->GetNumberOfPoints() != 0)
+    {
+      itkExceptionMacro(<<"Input point sets set size differs.");
+    }
+  
+  MetricType::Pointer metric = MetricType::New();
+  TransformationPointerType invTransform = TransformationType::New();
+  // Optimizer Type
+  OptimizerType::Pointer optimizer = OptimizerType::New();
+  optimizer->SetUseCostFunctionGradient(false);
+  invTransform->SetIdentity();
+  RegistrationType::Pointer registration = RegistrationType::New();
+  // Scale the translation components of the Transform in the Optimizer
+  
+  OptimizerType::ScalesType scales( invTransform->GetNumberOfParameters() );
+
+  scales.Fill( 0.01 );
+  unsigned long numberOfIterations = 100;
+  double gradientTolerance = 1e-5; // convergence criterion
+  double valueTolerance = 1e-5; // convergence criterion
+  double epsilonFunction = 1e-6; // convergence criterion
+  optimizer->SetScales( scales );
+  optimizer->SetNumberOfIterations( numberOfIterations );
+  optimizer->SetValueTolerance( valueTolerance );
+  optimizer->SetGradientTolerance( gradientTolerance );
+  optimizer->SetEpsilonFunction( epsilonFunction );
+
+  registration->SetInitialTransformParameters( invTransform->GetParameters() );
+
+  //------------------------------------------------------
+  // Connect all the components required for Registration
+  //------------------------------------------------------
+  registration->SetMetric( metric );
+  registration->SetOptimizer( optimizer );
+  registration->SetTransform( invTransform );
+  registration->SetFixedPointSet( m_FixPointSet ); 
+  registration->SetMovingPointSet( m_MovingPointSet );
+
+  registration->StartRegistration();
+
+ invTransform->GetInverse( m_Transform );
+
+ std::cout << "Solution = " << m_Transform->GetParameters() << std::endl;
+ m_TransformParameters = m_Transform->GetParameters();
+}
+
+
+}// namespace otb
+
+#endif
