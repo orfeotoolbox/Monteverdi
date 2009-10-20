dd91a8ae3de102d21f29d88ef27840909c911d6d
Julien Michel <julien.michel@orfeo-toolbox.org>
2009-10-20 10:47:16 +0200

    STY: Customizing icons

---
+++ Code/Application/otbMonteverdiPixmaps.h
@@ -0,0 +1,34 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __otbMonteverdiPixmaps_h
+#define __otbMonteverdiPixmaps_h
+
+#include "FLU/flu_export.h"
+
+namespace otb
+{
+
+FLU_EXPORT extern const char* vectorImage_xpm[];
+FLU_EXPORT extern const char* scalarImage_xpm[];
+FLU_EXPORT extern const char* vectorData_xpm[];
+FLU_EXPORT extern const char* process_xpm[];
+
+
+} // end namespace otb
+
+#endif
