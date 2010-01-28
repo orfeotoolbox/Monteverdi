21eaab50f913a758bd3a6fa87118427427530d28
Etienne Bougoin <etienne.bougoin@c-s.fr>
2010-01-28 11:32:12 +0100

    ENH : Add select area in extract ROI module

--- Code/Modules/ExtractROI/otbExtractROIModule.cxx
+++ Code/Modules/ExtractROI/otbExtractROIModule.cxx
@@ -1,20 +1,20 @@
 /*=========================================================================
-
-  Program:   ORFEO Toolbox
-  Language:  C++
-  Date:      $Date$
-  Version:   $Revision$
-
-
-  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
-  See OTBCopyright.txt for details.
-
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
-
-=========================================================================*/
+ 
+ Program:   ORFEO Toolbox
+ Language:  C++
+ Date:      $Date$
+ Version:   $Revision$
+ 
+ 
+ Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+ See OTBCopyright.txt for details.
+ 
+ 
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
+ 
+ =========================================================================*/
 
 #include "otbExtractROIModule.h"
 #include "otbMsgReporter.h"
@@ -24,49 +24,94 @@
 
 namespace otb
 {
-/** Constructor */
-ExtractROIModule::ExtractROIModule()
-{
-  // Describe inputs
-  this->AddInputDescriptor<FloatingImageType>("InputImage", otbGetTextMacro("Image to read"));
-  this->AddTypeToInputDescriptor<FloatingVectorImageType>("InputImage");
-  m_VectorImageExtractROIFilter = VectorImageExtractROIFilterType::New();
-  m_ImageExtractROIFilter = ImageExtractROIFilterType::New();
-  m_Transform        = TransformType::New();
-  m_InverseTransform = TransformType::New();
-  this->BuildGUI();
-}
-
-/** Destructor */
-ExtractROIModule::~ExtractROIModule()
-{
-    this->Cancel();
-}
-
-/** PrintSelf method */
-void ExtractROIModule::PrintSelf(std::ostream& os, itk::Indent indent) const
-{
-  // Call superclass implementation
-  Superclass::PrintSelf(os,indent);
-}
-
-/** The custom run command */
-void ExtractROIModule::Run()
-{
-  FloatingImageType::Pointer image = this->GetInputData<FloatingImageType>("InputImage");
-  FloatingVectorImageType::Pointer vectorImage = this->GetInputData<FloatingVectorImageType>("InputImage");
-
-  itk::ImageRegion<2> imageRegion;
-
-  typedef ForwardSensorModel<double>                ForwardSensorType;
-
-  roundLongLat->activate();
-
-  if(!image.IsNull() && vectorImage.IsNull())
+  /** Constructor */
+  ExtractROIModule::ExtractROIModule()
   {
+    // Describe inputs
+    this->AddInputDescriptor<FloatingImageType>("InputImage", otbGetTextMacro("Image to read"));
+    this->AddTypeToInputDescriptor<FloatingVectorImageType>("InputImage");
+    m_VectorImageExtractROIFilter = VectorImageExtractROIFilterType::New();
+    m_ImageExtractROIFilter       = ImageExtractROIFilterType::New();
+    m_Transform                   = TransformType::New();
+    m_InverseTransform            = TransformType::New();
+    
+    m_Model                       = ModelType::New();
+    m_View                        = ViewType::New();
+    m_Controller                  = ControllerType::New();
+    m_ResizingHandler             = ResizingHandlerType::New();
+    m_SelectAreaHandler           = SelectAreaHandlerType::New();
+    m_regionGl                    = RegionGlComponentType::New();
+    
+    m_View->SetModel(m_Model);
+    m_View->SetController(m_Controller);
+    
+    // RegionGlComponent for select area
+    ColorType color;
+    color[0] = 0;
+    color[1] = 1;
+    color[2] = 0;
+    color[3] = 1;
+    m_regionGl->SetColor(color);
+    m_View->GetScrollWidget()->AddGlComponent(m_regionGl);
+    
+    // Add the select area handler
+    m_SelectAreaHandler->SetModel(m_Model);
+    m_SelectAreaHandler->SetWidget(m_View->GetScrollWidget());
+    m_SelectAreaHandler->SetRegionGlComponent(m_regionGl);
+    m_Controller->AddActionHandler(m_SelectAreaHandler);
+    
+    // Add the resizing handler
+    m_ResizingHandler->SetModel(m_Model);
+    m_ResizingHandler->SetView(m_View);
+    m_Controller->AddActionHandler(m_ResizingHandler);
+    
+    this->BuildGUI();
+    
+    m_SelectAreaHandler->SetStartX(vStartX);
+    m_SelectAreaHandler->SetStartY(vStartY);
+    m_SelectAreaHandler->SetSizeX(vSizeX);
+    m_SelectAreaHandler->SetSizeY(vSizeY);
+    
+    vView->add(m_View->GetScrollWidget());
+    vView->resizable(m_View->GetScrollWidget());
+    m_View->GetScrollWidget()->resize(vView->x()+1,vView->y()+1, vView->w()-2, vView->h()-2);
+  }
+  
+  /** Destructor */
+  ExtractROIModule::~ExtractROIModule()
+  {
+    this->Cancel();
+  }
+  
+  /** PrintSelf method */
+  void ExtractROIModule::PrintSelf(std::ostream& os, itk::Indent indent) const
+  {
+    // Call superclass implementation
+    Superclass::PrintSelf(os,indent);
+  }
+  
+  /** The custom run command */
+  void ExtractROIModule::Run()
+  {
+    FloatingImageType::Pointer image              = this->GetInputData<FloatingImageType>("InputImage");
+    FloatingVectorImageType::Pointer vectorImage  = this->GetInputData<FloatingVectorImageType>("InputImage");
+    
+    itk::ImageRegion<2>                 imageRegion;
+    
+    typedef ForwardSensorModel<double>  ForwardSensorType;
+    
+    roundLongLat->activate();
+    
+    if(!image.IsNull() && vectorImage.IsNull())
+    {
       image->UpdateOutputInformation();
       imageRegion = image->GetLargestPossibleRegion();
-
+      
+      m_Generator = LayerGeneratorType::New();
+      m_Generator->SetImage(image);
+      m_Generator->GenerateLayer();
+      m_Model->AddLayer(m_Generator->GetLayer());
+      
       try
       {
         const itk::MetaDataDictionary & inputDict = image->GetMetaDataDictionary();
@@ -74,7 +119,7 @@ void ExtractROIModule::Run()
         m_Transform->SetInputOrigin(image->GetOrigin());
         m_Transform->SetInputSpacing(image->GetSpacing());
         m_Transform->InstanciateTransform();
-
+        
         if (m_Transform->GetTransformAccuracy() == Projection::UNKNOWN)
         {
           roundLongLat->deactivate();
@@ -84,12 +129,18 @@ void ExtractROIModule::Run()
       {
         roundLongLat->deactivate();
       }
-  }
-  else if(image.IsNull() && !vectorImage.IsNull())
-  {
+    }
+    else if(image.IsNull() && !vectorImage.IsNull())
+    {
       vectorImage->UpdateOutputInformation();
       imageRegion = vectorImage->GetLargestPossibleRegion();
-
+      
+      /** Add view */
+      m_VectorGenerator = VectorLayerGeneratorType::New();
+      m_VectorGenerator->SetImage(vectorImage);
+      m_VectorGenerator->GenerateLayer();
+      m_Model->AddLayer(m_VectorGenerator->GetLayer());
+      
       try
       {
         const itk::MetaDataDictionary & inputDict = vectorImage->GetMetaDataDictionary();
@@ -97,7 +148,7 @@ void ExtractROIModule::Run()
         m_Transform->SetInputOrigin(vectorImage->GetOrigin());
         m_Transform->SetInputSpacing(vectorImage->GetSpacing());
         m_Transform->InstanciateTransform();
-
+        
         if (m_Transform->GetTransformAccuracy() == Projection::UNKNOWN)
         {
           roundLongLat->deactivate();
@@ -107,102 +158,114 @@ void ExtractROIModule::Run()
       {
         roundLongLat->deactivate();
       }
+    }
+    else
+    {
+      itkExceptionMacro("The image pointer is not initialized!!");
+    }
+    
+    m_SelectAreaHandler->SetLargestRegion(imageRegion);
+    
+    m_regionGl->SetRegion(imageRegion);
+    
+    vInputImageSizeX->value(imageRegion.GetSize()[0]);
+    vInputImageSizeY->value(imageRegion.GetSize()[1]);
+    
+    vStartX->minimum(static_cast<double>(imageRegion.GetIndex()[0]));
+    vStartX->maximum(static_cast<double>(imageRegion.GetSize()[0]));
+    vStartX->value(vStartX->minimum());
+    vStartY->minimum(static_cast<double>(imageRegion.GetIndex()[1]));
+    vStartY->maximum(static_cast<double>(imageRegion.GetSize()[1]));
+    vStartY->value(vStartY->minimum());
+    
+    vSizeX->minimum(vStartX->minimum());
+    vSizeX->maximum(vStartX->maximum());
+    vSizeX->value(vSizeX->maximum());
+    vSizeY->minimum(vStartY->minimum());
+    vSizeY->maximum(vStartY->maximum());
+    vSizeY->value(vSizeY->maximum());
+    
+    wExtractROIWindow->show();
+    m_View->GetScrollWidget()->show();
+    m_Model->Update();
   }
-  else
+  
+  
+  void ExtractROIModule::LongLatSelection()
   {
-    itkExceptionMacro("The image pointer is not initialized!!");
+    if ( roundLongLat->value() == 1 )
+    {
+      guiPixel->hide();
+      guiLongLat->show();
+    }
+    else
+    {
+      guiPixel->show();
+      guiLongLat->hide();
+    }
   }
-
-  vInputImageSizeX->value(imageRegion.GetSize()[0]);
-  vInputImageSizeY->value(imageRegion.GetSize()[1]);
-
-  vStartX->minimum(static_cast<double>(imageRegion.GetIndex()[0]));
-  vStartX->maximum(static_cast<double>(imageRegion.GetSize()[0]));
-  vStartX->value(vStartX->minimum());
-  vStartY->minimum(static_cast<double>(imageRegion.GetIndex()[1]));
-  vStartY->maximum(static_cast<double>(imageRegion.GetSize()[1]));
-  vStartY->value(vStartY->minimum());
-
-  vSizeX->minimum(vStartX->minimum());
-  vSizeX->maximum(vStartX->maximum());
-  vSizeX->value(vSizeX->maximum());
-  vSizeY->minimum(vStartY->minimum());
-  vSizeY->maximum(vStartY->maximum());
-  vSizeY->value(vSizeY->maximum());
-  wExtractROIWindow->show();
-}
-
-
-void ExtractROIModule::LongLatSelection()
-{
-  if ( roundLongLat->value() == 1 )
+  
+  void ExtractROIModule::UpdateRegion()
   {
-    guiPixel->hide();
-    guiLongLat->show();
+    m_SelectAreaHandler->UpdateGlRegion();
   }
-  else
+  
+  /** Cancel */
+  void ExtractROIModule::Cancel()
   {
-    guiPixel->show();
-    guiLongLat->hide();
-  }
-}
-
-/** Cancel */
-void ExtractROIModule::Cancel()
-{
     wExtractROIWindow->hide();
-}
-/** Ok */
-void ExtractROIModule::Ok()
-{
-  try
+  }
+  /** Ok */
+  void ExtractROIModule::Ok()
+  {
+    try
     {
       FloatingImageType::Pointer image = this->GetInputData<FloatingImageType>("InputImage");
       FloatingVectorImageType::Pointer vectorImage = this->GetInputData<FloatingVectorImageType>("InputImage");
-
+      
       IndexType idxInit;
       OffsetType offSize;
       if(!image.IsNull() && vectorImage.IsNull())
       {
         // Get input image
         FloatingImageType::Pointer image = this->GetInputData<FloatingImageType>("InputImage");
-
+        
         //TODO Add case of long/lat input
         if ( roundLongLat->value() == 1 )
         {
           OutputPointType pt1;
           OutputPointType pt2;
-
+          
           pt1[0] = static_cast<InternalPixelType>( vLong1->value() );
           pt1[1] = static_cast<InternalPixelType>( vLatitude1->value() );
           pt2[0] = static_cast<InternalPixelType>( vLong2->value() );
           pt2[1] = static_cast<InternalPixelType>( vLatitude2->value() );
-
+          
           OutputPointType pto1;
           OutputPointType pto2;
           m_Transform->GetInverse(m_InverseTransform);
           pto1 = m_InverseTransform->TransformPoint(pt1);
           pto2 = m_InverseTransform->TransformPoint(pt2);
-
+          
           IndexType index1;
           IndexType index2;
-
+          
           image->TransformPhysicalPointToIndex(pto1, index1);
           image->TransformPhysicalPointToIndex(pto2, index2);
-
+          
           idxInit = index1;
           offSize = index2 - index1;
-
+          
         }
         else
         {
           idxInit[0] = static_cast<unsigned long>(vStartX->value());
           idxInit[1] = static_cast<unsigned long>(vStartY->value());
-
+          
           offSize[0] = static_cast<unsigned long>(vSizeX->value());
           offSize[1] = static_cast<unsigned long>(vSizeY->value());
         }
-
+        
         m_ImageExtractROIFilter->SetStartX(idxInit[0]);
         m_ImageExtractROIFilter->SetStartY(idxInit[1]);
         m_ImageExtractROIFilter->SetSizeX(offSize[0]);
@@ -213,34 +276,34 @@ void ExtractROIModule::Ok()
         this->NotifyOutputsChange();
       }
       else if(image.IsNull() && !vectorImage.IsNull())
-
+        
       {
         // Get Input Vector Image
         FloatingVectorImageType::Pointer vectorImage = this->GetInputData<FloatingVectorImageType>("InputImage");
-
+        
         //TODO Add case of long/lat input
         if ( roundLongLat->value() == 1 )
         {
           OutputPointType pt1;
           OutputPointType pt2;
-
+          
           pt1[0] = static_cast<InternalPixelType>( vLong1->value() );
           pt1[1] = static_cast<InternalPixelType>( vLatitude1->value() );
           pt2[0] = static_cast<InternalPixelType>( vLong2->value() );
           pt2[1] = static_cast<InternalPixelType>( vLatitude2->value() );
-
+          
           OutputPointType pto1;
           OutputPointType pto2;
           m_Transform->GetInverse(m_InverseTransform);
           pto1 = m_InverseTransform->TransformPoint(pt1);
           pto2 = m_InverseTransform->TransformPoint(pt2);
-
+          
           IndexType index1;
           IndexType index2;
-
+          
           vectorImage->TransformPhysicalPointToIndex(pto1, index1);
           vectorImage->TransformPhysicalPointToIndex(pto2, index2);
-
+          
           idxInit = index1;
           offSize = index2 - index1;
         }
@@ -248,11 +311,11 @@ void ExtractROIModule::Ok()
         {
           idxInit[0] = static_cast<unsigned long>(vStartX->value());
           idxInit[1] = static_cast<unsigned long>(vStartY->value());
-
+          
           offSize[0] = static_cast<unsigned long>(vSizeX->value());
           offSize[1] = static_cast<unsigned long>(vSizeY->value());
         }
-
+        
         m_VectorImageExtractROIFilter->SetStartX(idxInit[0]);
         m_VectorImageExtractROIFilter->SetStartY(idxInit[1]);
         m_VectorImageExtractROIFilter->SetSizeX(offSize[0]);
@@ -262,15 +325,15 @@ void ExtractROIModule::Ok()
         this->AddOutputDescriptor(m_VectorImageExtractROIFilter->GetOutput(),"OutputImage", otbGetTextMacro("Image extracted") );
         this->NotifyOutputsChange();
       }
-
-     wExtractROIWindow->hide();
+      
+      wExtractROIWindow->hide();
     }
-  catch(itk::ExceptionObject & err)
+    catch(itk::ExceptionObject & err)
     {
-    MsgReporter::GetInstance()->SendError(err.GetDescription());
+      MsgReporter::GetInstance()->SendError(err.GetDescription());
     }
-}
-
+  }
+  
 } // End namespace otb
 
 
--- Code/Modules/ExtractROI/otbExtractROIModule.h
+++ Code/Modules/ExtractROI/otbExtractROIModule.h
@@ -1,20 +1,20 @@
 /*=========================================================================
-
-  Program:   ORFEO Toolbox
-  Language:  C++
-  Date:      $Date$
-  Version:   $Revision$
-
-
-  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
-  See OTBCopyright.txt for details.
-
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
-
-=========================================================================*/
+ 
+ Program:   ORFEO Toolbox
+ Language:  C++
+ Date:      $Date$
+ Version:   $Revision$
+ 
+ 
+ Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+ See OTBCopyright.txt for details.
+ 
+ 
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notices for more information.
+ 
+ =========================================================================*/
 #ifndef __otbExtractROIModule_h
 #define __otbExtractROIModule_h
 
@@ -30,95 +30,147 @@
 #include "otbExtractROI.h"
 #include "otbMultiChannelExtractROI.h"
 
+#include "itkRGBPixel.h"
+#include "otbImageLayerGenerator.h"
+#include "otbImageLayer.h"
+#include "otbImageView.h"
+#include "otbImageLayerRenderingModel.h"
+#include "otbImageWidgetController.h"
+
+#include "otbWidgetResizingActionHandler.h"
+#include "otbSelectAreaROIActionHandler.h"
+
 #include "otbInverseSensorModel.h"
 #include "otbGenericRSTransform.h"
 namespace otb
 {
-/** \class ExtractROIModule
- *  \brief
- *
- *  \sa DataObjectWrapper, DataDescriptor, DataDescriptor
- */
-
-class ITK_EXPORT ExtractROIModule
+  /** \class ExtractROIModule
+   *  \brief
+   *
+   *  \sa DataObjectWrapper, DataDescriptor, DataDescriptor
+   */
+  
+  class ITK_EXPORT ExtractROIModule
   : public Module, public ExtractROIModuleGUI
-{
-public:
-  /** Standard class typedefs */
-  typedef ExtractROIModule                 Self;
-  typedef Module                        Superclass;
-  typedef itk::SmartPointer<Self>       Pointer;
-  typedef itk::SmartPointer<const Self> ConstPointer;
-
-  /** New macro */
-  itkNewMacro(Self);
-
-  /** Type macro */
-  itkTypeMacro(ExtractROIModule,Module);
-
-  /** OTB typedefs */
-  typedef double                                  InternalPixelType;
-  /// Dataset
-  typedef Image<InternalPixelType,2>              FloatingImageType;
-  typedef VectorImage<InternalPixelType,2>        FloatingVectorImageType;
-  /// Multi channels Extract ROI filter
-  typedef MultiChannelExtractROI< InternalPixelType, InternalPixelType >  VectorImageExtractROIFilterType;
-  /// Mono channel Extract ROI filter
-  typedef ExtractROI< InternalPixelType, InternalPixelType >  ImageExtractROIFilterType;
-  
-  /** Inverse sensor model typedefs */
-  typedef InverseSensorModel<double>                InverseSensorType;
-  typedef InverseSensorType::InputPointType         InverseSensorInputPointType;
-  typedef InverseSensorType::OutputPointType        InverseSensorOutputPointType;
-    
-  typedef GenericRSTransform<>                        TransformType;
-  typedef TransformType::OutputPointType              OutputPointType;
-  
-  /** Typedef to cast physical point to index*/
-  typedef FloatingImageType::IndexType             IndexType;
-  typedef IndexType::OffsetType             OffsetType;
-  typedef FloatingImageType::PointType             PointType;
-//   typedef FloatingVectorImageType::IndexType             FloatingVectorImageIndexType;
-  /** Show the Module GUI */
-  virtual bool CanShow(){return true;};
-  /** Show the Module GUI */
-  virtual void Show()
   {
-    wExtractROIWindow->show();
+  public:
+    /** Standard class typedefs */
+    typedef ExtractROIModule                                                Self;
+    typedef Module                                                          Superclass;
+    typedef itk::SmartPointer<Self>                                         Pointer;
+    typedef itk::SmartPointer<const Self>                                   ConstPointer;
+    
+    /** New macro */
+    itkNewMacro(Self);
+    
+    /** Type macro */
+    itkTypeMacro(ExtractROIModule,Module);
+    
+    /** OTB typedefs */
+    typedef double                                                          InternalPixelType;
+    /// Dataset
+    typedef Image<InternalPixelType,2>                                      FloatingImageType;
+    typedef VectorImage<InternalPixelType,2>                                FloatingVectorImageType;
+    /// Multi channels Extract ROI filter
+    typedef MultiChannelExtractROI< InternalPixelType, InternalPixelType >  VectorImageExtractROIFilterType;
+    /// Mono channel Extract ROI filter
+    typedef ExtractROI< InternalPixelType, InternalPixelType >              ImageExtractROIFilterType;
+    
+    /** Inverse sensor model typedefs */
+    typedef InverseSensorModel<double>                                      InverseSensorType;
+    typedef InverseSensorType::InputPointType                               InverseSensorInputPointType;
+    typedef InverseSensorType::OutputPointType                              InverseSensorOutputPointType;
+    
+    typedef GenericRSTransform<>                                            TransformType;
+    typedef TransformType::OutputPointType                                  OutputPointType;
+    
+    /** Typedef to cast physical point to index*/
+    typedef FloatingImageType::IndexType                                    IndexType;
+    typedef IndexType::OffsetType                                           OffsetType;
+    typedef FloatingImageType::PointType                                    PointType;
+    
+    /** Typedefs to add view */
+    typedef itk::RGBPixel<unsigned char>                                    RGBPixelType;
+    typedef otb::Image<RGBPixelType,2>                                      OutputImageType;
+    typedef otb::ImageLayerRenderingModel<OutputImageType>                  ModelType;
+    typedef otb::ImageView<ModelType>                                       ViewType;
+    typedef otb::ImageWidgetController                                      ControllerType;
+    
+    typedef otb::ImageLayer<FloatingImageType, OutputImageType>             LayerType;
+    typedef otb::ImageLayerGenerator<LayerType>                             LayerGeneratorType;
+    
+    typedef otb::ImageLayer<FloatingVectorImageType, OutputImageType>       VectorLayerType;
+    typedef otb::ImageLayerGenerator<VectorLayerType>                       VectorLayerGeneratorType;
+    
+    typedef otb::WidgetResizingActionHandler<ModelType,ViewType>            ResizingHandlerType;
+    
+    typedef ViewType::ImageWidgetType                                       WidgetType;
+    typedef ViewType::RegionGlComponentType                                 RegionGlComponentType;
+    typedef RegionGlComponentType::ColorType                                ColorType;
+    typedef otb::SelectAreaROIActionHandler<ModelType, WidgetType>          SelectAreaHandlerType;
+    
+    /** Show the Module GUI */
+    virtual bool CanShow(){return true;};
+    /** Show the Module GUI */
+    virtual void Show()
+    {
+      wExtractROIWindow->show();
+    };
+    
+    virtual void LongLatSelection();
+    
+  protected:
+    /** Constructor */
+    ExtractROIModule();
+    /** Destructor */
+    virtual ~ExtractROIModule();
+    /** PrintSelf method */
+    virtual void PrintSelf(std::ostream& os, itk::Indent indent) const;
+    
+    /** The custom run command */
+    virtual void Run();
+    
+    /** Callbacks */
+    virtual void Ok();
+    virtual void Cancel();
+    virtual void UpdateRegion();
+    
+  private:
+    
+    ExtractROIModule(const Self&); //purposely not implemented
+    void operator=(const Self&); //purposely not implemented
+    
+    /** Pointer to the image extract ROI filter object */
+    ImageExtractROIFilterType::Pointer        m_ImageExtractROIFilter;
+    /** Pointer to the vector image extract ROI filter object */
+    VectorImageExtractROIFilterType::Pointer  m_VectorImageExtractROIFilter;
+    
+    TransformType::Pointer                    m_Transform;
+    TransformType::Pointer                    m_InverseTransform;
+    
+    
+    /** Pointer to the Model */
+    ModelType::Pointer                        m_Model;
+    
+    /** Pointer to the Layer generator */
+    LayerGeneratorType::Pointer               m_Generator;
+    VectorLayerGeneratorType::Pointer         m_VectorGenerator;
+    
+    /** Pointer to the view */
+    ViewType::Pointer                         m_View;
+    
+    /** Pointer to the controller */
+    ControllerType::Pointer                   m_Controller;
+    
+    /** Pointer to the handlers */
+    ResizingHandlerType::Pointer              m_ResizingHandler;
+    SelectAreaHandlerType::Pointer            m_SelectAreaHandler;
+    
+    // Region for select area
+    RegionGlComponentType::Pointer            m_regionGl;
+        
   };
   
-  virtual void LongLatSelection();
-
-protected:
-  /** Constructor */
-  ExtractROIModule();
-  /** Destructor */
-  virtual ~ExtractROIModule();
-  /** PrintSelf method */
-  virtual void PrintSelf(std::ostream& os, itk::Indent indent) const;
-
-  /** The custom run command */
-  virtual void Run();
-
-  /** Callbacks */
-  virtual void Ok();
-  virtual void Cancel();
-
-private:
-
-  ExtractROIModule(const Self&); //purposely not implemented
-  void operator=(const Self&); //purposely not implemented
-
-  /** Pointer to the image extract ROI filter object */
-  ImageExtractROIFilterType::Pointer m_ImageExtractROIFilter;
-  /** Pointer to the vector image extract ROI filter object */
-  VectorImageExtractROIFilterType::Pointer m_VectorImageExtractROIFilter;
-  
-  TransformType::Pointer   m_Transform;
-  TransformType::Pointer   m_InverseTransform;
-
-};
-
 } // End namespace otb
 
 #endif
