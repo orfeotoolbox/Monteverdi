de7a8320f49ea0523720be43e5c344785e1774d7
Julien Malik <julien.malik@orfeo-toolbox.org>
2010-06-18 14:48:59 +0200

    ENH: add signed int/short support, simplify code and remove autoscale checkbox

--- Code/Modules/Writer/otbWriterModule.cxx
+++ Code/Modules/Writer/otbWriterModule.cxx
@@ -36,14 +36,18 @@ WriterModule::WriterModule()
   // Image inputs
   this->AddInputDescriptor<UCharImageType>(InputDataSetID,otbGetTextMacro("Dataset to write"));
   this->AddTypeToInputDescriptor<UShortImageType>(InputDataSetID);
+  this->AddTypeToInputDescriptor<ShortImageType>(InputDataSetID);
   this->AddTypeToInputDescriptor<UIntImageType>(InputDataSetID);
+  this->AddTypeToInputDescriptor<IntImageType>(InputDataSetID);
   this->AddTypeToInputDescriptor<FloatImageType>(InputDataSetID);
   this->AddTypeToInputDescriptor<DoubleImageType>(InputDataSetID);
 
   // Vector Image inputs
   this->AddTypeToInputDescriptor<UCharVectorImageType>(InputDataSetID);
   this->AddTypeToInputDescriptor<UShortVectorImageType>(InputDataSetID);
+  this->AddTypeToInputDescriptor<ShortVectorImageType>(InputDataSetID);
   this->AddTypeToInputDescriptor<UIntVectorImageType>(InputDataSetID);
+  this->AddTypeToInputDescriptor<IntVectorImageType>(InputDataSetID);
   this->AddTypeToInputDescriptor<FloatVectorImageType>(InputDataSetID);
   this->AddTypeToInputDescriptor<DoubleVectorImageType>(InputDataSetID);
 
@@ -51,11 +55,13 @@ WriterModule::WriterModule()
   this->AddTypeToInputDescriptor<VectorType>(InputDataSetID);
   this->AddTypeToInputDescriptor<LabeledVectorType>(InputDataSetID);
 
-  m_OutputTypesChoices[UCHAR]  = "unsigned char";
-  m_OutputTypesChoices[USHORT] = "unsigned short";
-  m_OutputTypesChoices[UINT]   = "unsigned int";
-  m_OutputTypesChoices[FLOAT]  = "float";
-  m_OutputTypesChoices[DOUBLE] = "double";
+  m_OutputTypesChoices[otb::UNSIGNEDCHAR]     = "unsigned char (8 bits)";
+  m_OutputTypesChoices[otb::SHORTINT]         = "short (16 bits)";
+  m_OutputTypesChoices[otb::INT]              = "int (32 bits)";
+  m_OutputTypesChoices[otb::FLOAT]            = "float (32 bits)";
+  m_OutputTypesChoices[otb::DOUBLE]           = "double (64 bits)";
+  m_OutputTypesChoices[otb::UNSIGNEDSHORTINT] = "unsigned short (16 bits)";
+  m_OutputTypesChoices[otb::UNSIGNEDINT]      = "unsigned int (32 bits)";
 }
 
 /** Destructor */
@@ -75,45 +81,53 @@ void WriterModule::Run()
 {
   this->BuildGUI();
 
-  // TODO hide this widget until we know what we do with it
-  bAutoScale->hide();
-
-  cOutDataType->add(m_OutputTypesChoices[UCHAR].c_str());
-  cOutDataType->add(m_OutputTypesChoices[USHORT].c_str());
-  cOutDataType->add(m_OutputTypesChoices[UINT].c_str());
-  cOutDataType->add(m_OutputTypesChoices[FLOAT].c_str());
-  cOutDataType->add(m_OutputTypesChoices[DOUBLE].c_str());
+  cOutDataType->add(m_OutputTypesChoices[otb::UNSIGNEDCHAR].c_str());
+  cOutDataType->add(m_OutputTypesChoices[otb::SHORTINT].c_str());
+  cOutDataType->add(m_OutputTypesChoices[otb::INT].c_str());
+  cOutDataType->add(m_OutputTypesChoices[otb::FLOAT].c_str());
+  cOutDataType->add(m_OutputTypesChoices[otb::DOUBLE].c_str());
+  cOutDataType->add(m_OutputTypesChoices[otb::UNSIGNEDSHORTINT].c_str());
+  cOutDataType->add(m_OutputTypesChoices[otb::UNSIGNEDINT].c_str());
 
   if ( this->GetInputData<UCharImageType>(InputDataSetID) != 0
        || this->GetInputData<UCharVectorImageType>(InputDataSetID) != 0 )
     {
-    cOutDataType->value(UCHAR);
+    cOutDataType->value(otb::UNSIGNEDCHAR);
     }
-  else if ( this->GetInputData<UShortImageType>(InputDataSetID) != 0
-      || this->GetInputData<UShortVectorImageType>(InputDataSetID) != 0 )
+  else if ( this->GetInputData<ShortImageType>(InputDataSetID) != 0
+      || this->GetInputData<ShortVectorImageType>(InputDataSetID) != 0 )
     {
-    cOutDataType->value(USHORT);
+    cOutDataType->value(otb::SHORTINT);
     }
-  else if ( this->GetInputData<UShortImageType>(InputDataSetID) != 0
-      || this->GetInputData<UShortVectorImageType>(InputDataSetID) != 0 )
+  else if ( this->GetInputData<IntImageType>(InputDataSetID) != 0
+      || this->GetInputData<IntVectorImageType>(InputDataSetID) != 0 )
     {
-    cOutDataType->value(UINT);
+    cOutDataType->value(otb::INT);
     }
   else if ( this->GetInputData<FloatImageType>(InputDataSetID) != 0
       || this->GetInputData<FloatVectorImageType>(InputDataSetID) != 0 )
     {
-    cOutDataType->value(FLOAT);
+    cOutDataType->value(otb::FLOAT);
     }
   else if ( this->GetInputData<DoubleImageType>(InputDataSetID) != 0
       || this->GetInputData<DoubleVectorImageType>(InputDataSetID) != 0 )
     {
-    cOutDataType->value(DOUBLE);
+    cOutDataType->value(otb::DOUBLE);
+    }
+  else if ( this->GetInputData<UShortImageType>(InputDataSetID) != 0
+      || this->GetInputData<UShortVectorImageType>(InputDataSetID) != 0 )
+    {
+    cOutDataType->value(otb::UNSIGNEDSHORTINT);
+    }
+  else if ( this->GetInputData<UIntImageType>(InputDataSetID) != 0
+      || this->GetInputData<UIntVectorImageType>(InputDataSetID) != 0 )
+    {
+    cOutDataType->value(otb::UNSIGNEDINT);
     }
   else
     {
     // Vector Data
     cOutDataType->hide();
-    bAutoScale->hide();
     }
 
   pBar->minimum(0);
@@ -223,241 +237,69 @@ void WriterModule::ThreadedRun()
   this->BusyOn();
 
   m_Filename = vFilePath->value();
-  m_AutoScale = (bAutoScale->value() == 1);
-  OutputFormat outFormat = static_cast<OutputFormat> (cOutDataType->value());
+  PixelType outFormat = static_cast<PixelType> (cOutDataType->value());
 
   try
     {
+    // Mono band images
     if (this->GetInputData<UCharImageType> (InputDataSetID))
       {
-        UCharImageType::Pointer image = this->GetInputData<UCharImageType> (InputDataSetID);
-        switch (outFormat) {
-        case UCHAR:
-          this->DoWrite<UCharImageType, UCharImageType> (image);
-          break;
-        case USHORT:
-          this->DoWrite<UCharImageType, UShortImageType> (image);
-          break;
-        case UINT:
-          this->DoWrite<UCharImageType, UIntImageType> (image);
-          break;
-        case FLOAT:
-          this->DoWrite<UCharImageType, FloatImageType> (image);
-          break;
-        case DOUBLE:
-          this->DoWrite<UCharImageType, DoubleImageType> (image);
-          break;
-        default:
-          break;
-        }
+      this->DoWriteSingleBand(this->GetInputData<UCharImageType> (InputDataSetID), outFormat);
       }
     else if (this->GetInputData<UShortImageType> (InputDataSetID))
       {
-        UShortImageType::Pointer image = this->GetInputData<UShortImageType> (InputDataSetID);
-        switch (outFormat) {
-        case UCHAR:
-          this->DoWrite<UShortImageType, UCharImageType> (image);
-          break;
-        case USHORT:
-          this->DoWrite<UShortImageType, UShortImageType> (image);
-          break;
-        case UINT:
-          this->DoWrite<UShortImageType, UIntImageType> (image);
-          break;
-        case FLOAT:
-          this->DoWrite<UShortImageType, FloatImageType> (image);
-          break;
-        case DOUBLE:
-          this->DoWrite<UShortImageType, DoubleImageType> (image);
-          break;
-        default:
-          break;
-        }
+      this->DoWriteSingleBand(this->GetInputData<UShortImageType> (InputDataSetID), outFormat);
       }
     else if (this->GetInputData<UIntImageType> (InputDataSetID))
       {
-        UIntImageType::Pointer image = this->GetInputData<UIntImageType> (InputDataSetID);
-        switch (outFormat) {
-        case UCHAR:
-          this->DoWrite<UIntImageType, UCharImageType> (image);
-          break;
-        case USHORT:
-          this->DoWrite<UIntImageType, UShortImageType> (image);
-          break;
-        case UINT:
-          this->DoWrite<UIntImageType, UIntImageType> (image);
-          break;
-        case FLOAT:
-          this->DoWrite<UIntImageType, FloatImageType> (image);
-          break;
-        case DOUBLE:
-          this->DoWrite<UIntImageType, DoubleImageType> (image);
-          break;
-        default:
-          break;
-        }
+      this->DoWriteSingleBand(this->GetInputData<UIntImageType> (InputDataSetID), outFormat);
       }
     else if (this->GetInputData<FloatImageType> (InputDataSetID))
       {
-        FloatImageType::Pointer image = this->GetInputData<FloatImageType> (InputDataSetID);
-        switch (outFormat) {
-        case UCHAR:
-          this->DoWrite<FloatImageType, UCharImageType> (image);
-          break;
-        case USHORT:
-          this->DoWrite<FloatImageType, UShortImageType> (image);
-          break;
-        case UINT:
-          this->DoWrite<FloatImageType, UIntImageType> (image);
-          break;
-        case FLOAT:
-          this->DoWrite<FloatImageType, FloatImageType> (image);
-          break;
-        case DOUBLE:
-          this->DoWrite<FloatImageType, DoubleImageType> (image);
-          break;
-        default:
-          break;
-        }
+      this->DoWriteSingleBand(this->GetInputData<FloatImageType> (InputDataSetID), outFormat);
       }
     else if (this->GetInputData<DoubleImageType> (InputDataSetID))
       {
-        DoubleImageType::Pointer image = this->GetInputData<DoubleImageType> (InputDataSetID);
-        switch (outFormat) {
-        case UCHAR:
-          this->DoWrite<DoubleImageType, UCharImageType> (image);
-          break;
-        case USHORT:
-          this->DoWrite<DoubleImageType, UShortImageType> (image);
-          break;
-        case UINT:
-          this->DoWrite<DoubleImageType, UIntImageType> (image);
-          break;
-        case FLOAT:
-          this->DoWrite<DoubleImageType, FloatImageType> (image);
-          break;
-        case DOUBLE:
-          this->DoWrite<DoubleImageType, DoubleImageType> (image);
-          break;
-        default:
-          break;
-        }
+      this->DoWriteSingleBand(this->GetInputData<DoubleImageType> (InputDataSetID), outFormat);
       }
+    else if (this->GetInputData<ShortImageType> (InputDataSetID))
+      {
+      this->DoWriteSingleBand(this->GetInputData<ShortImageType> (InputDataSetID), outFormat);
+      }
+    else if (this->GetInputData<IntImageType> (InputDataSetID))
+      {
+      this->DoWriteSingleBand(this->GetInputData<IntImageType> (InputDataSetID), outFormat);
+      }
+    // Multi band images
     else if (this->GetInputData<UCharVectorImageType> (InputDataSetID))
       {
-        UCharVectorImageType::Pointer image = this->GetInputData<UCharVectorImageType> (InputDataSetID);
-        switch (outFormat) {
-        case UCHAR:
-          this->DoWrite<UCharVectorImageType, UCharVectorImageType> (image);
-          break;
-        case USHORT:
-          this->DoWrite<UCharVectorImageType, UShortVectorImageType> (image);
-          break;
-        case UINT:
-          this->DoWrite<UCharVectorImageType, UIntVectorImageType> (image);
-          break;
-        case FLOAT:
-          this->DoWrite<UCharVectorImageType, FloatVectorImageType> (image);
-          break;
-        case DOUBLE:
-          this->DoWrite<UCharVectorImageType, DoubleVectorImageType> (image);
-          break;
-        default:
-          break;
-        }
+      this->DoWriteMultiBand(this->GetInputData<UCharVectorImageType> (InputDataSetID), outFormat);
       }
     else if (this->GetInputData<UShortVectorImageType> (InputDataSetID))
       {
-        UShortVectorImageType::Pointer image = this->GetInputData<UShortVectorImageType> (InputDataSetID);
-        switch (outFormat) {
-        case UCHAR:
-          this->DoWrite<UShortVectorImageType, UCharVectorImageType> (image);
-          break;
-        case USHORT:
-          this->DoWrite<UShortVectorImageType, UShortVectorImageType> (image);
-          break;
-        case UINT:
-          this->DoWrite<UShortVectorImageType, UIntVectorImageType> (image);
-          break;
-        case FLOAT:
-          this->DoWrite<UShortVectorImageType, FloatVectorImageType> (image);
-          break;
-        case DOUBLE:
-          this->DoWrite<UShortVectorImageType, DoubleVectorImageType> (image);
-          break;
-        default:
-          break;
-        }
+      this->DoWriteMultiBand(this->GetInputData<UShortVectorImageType> (InputDataSetID), outFormat);
       }
     else if (this->GetInputData<UIntVectorImageType> (InputDataSetID))
       {
-        UIntVectorImageType::Pointer image = this->GetInputData<UIntVectorImageType> (InputDataSetID);
-        switch (outFormat) {
-        case UCHAR:
-          this->DoWrite<UIntVectorImageType, UCharVectorImageType> (image);
-          break;
-        case USHORT:
-          this->DoWrite<UIntVectorImageType, UShortVectorImageType> (image);
-          break;
-        case UINT:
-          this->DoWrite<UIntVectorImageType, UIntVectorImageType> (image);
-          break;
-        case FLOAT:
-          this->DoWrite<UIntVectorImageType, FloatVectorImageType> (image);
-          break;
-        case DOUBLE:
-          this->DoWrite<UIntVectorImageType, DoubleVectorImageType> (image);
-          break;
-        default:
-          break;
-        }
+      this->DoWriteMultiBand(this->GetInputData<UIntVectorImageType> (InputDataSetID), outFormat);
       }
     else if (this->GetInputData<FloatVectorImageType> (InputDataSetID))
       {
-        FloatVectorImageType::Pointer image = this->GetInputData<FloatVectorImageType> (InputDataSetID);
-        switch (outFormat) {
-        case UCHAR:
-          this->DoWrite<FloatVectorImageType, UCharVectorImageType> (image);
-          break;
-        case USHORT:
-          this->DoWrite<FloatVectorImageType, UShortVectorImageType> (image);
-          break;
-        case UINT:
-          this->DoWrite<FloatVectorImageType, UIntVectorImageType> (image);
-          break;
-        case FLOAT:
-          this->DoWrite<FloatVectorImageType, FloatVectorImageType> (image);
-          break;
-        case DOUBLE:
-          this->DoWrite<FloatVectorImageType, DoubleVectorImageType> (image);
-          break;
-        default:
-          break;
-        }
+      this->DoWriteMultiBand(this->GetInputData<FloatVectorImageType> (InputDataSetID), outFormat);
       }
     else if (this->GetInputData<DoubleVectorImageType> (InputDataSetID))
       {
-        DoubleVectorImageType::Pointer image = this->GetInputData<DoubleVectorImageType> (InputDataSetID);
-        switch (outFormat) {
-        case UCHAR:
-          this->DoWrite<DoubleVectorImageType, UCharVectorImageType> (image);
-          break;
-        case USHORT:
-          this->DoWrite<DoubleVectorImageType, UShortVectorImageType> (image);
-          break;
-        case UINT:
-          this->DoWrite<DoubleVectorImageType, UIntVectorImageType> (image);
-          break;
-        case FLOAT:
-          this->DoWrite<DoubleVectorImageType, FloatVectorImageType> (image);
-          break;
-        case DOUBLE:
-          this->DoWrite<DoubleVectorImageType, DoubleVectorImageType> (image);
-          break;
-        default:
-          break;
-        }
+      this->DoWriteMultiBand(this->GetInputData<DoubleVectorImageType> (InputDataSetID), outFormat);
       }
+    else if (this->GetInputData<ShortVectorImageType> (InputDataSetID))
+      {
+      this->DoWriteMultiBand(this->GetInputData<ShortVectorImageType> (InputDataSetID), outFormat);
+      }
+    else if (this->GetInputData<IntVectorImageType> (InputDataSetID))
+      {
+      this->DoWriteMultiBand(this->GetInputData<IntVectorImageType> (InputDataSetID), outFormat);
+      }
+    // Vector Data
     else if( this->GetInputData<VectorType> (InputDataSetID) )
       {
       VectorWriterType::Pointer writer = VectorWriterType::New();
