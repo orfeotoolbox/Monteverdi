ac2495882e06fa9a2bc02af552fa9d6ff9f6ba4f
Otmane Lahlou <otmane.lahlou@c-s.fr>
2010-07-13 18:00:36 +0200

    ENH :  add the ObjectLabeling Module class

---
+++ Code/Modules/ObjectLabeling/otbObjectLabelingModule.cxx
@@ -0,0 +1,104 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "otbObjectLabelingModule.h"
+
+namespace otb
+{
+/** Constructor */
+ObjectLabelingModule::ObjectLabelingModule()
+{
+  // This module needs pipeline locking
+  this->NeedsPipelineLockingOff();
+
+  // First, do constructor stuffs
+
+  m_Controller = ControllerType::New();
+  m_View = ViewType::New();
+  m_Model = ModelType::New();
+
+  m_View->SetController(m_Controller);
+  m_View->SetModel(m_Model);
+  m_View->SetWidgetsController(m_Controller->GetWidgetsController());
+
+  m_Controller->SetModel(m_Model);
+  m_Controller->SetView(m_View);
+
+  m_Model->RegisterListener(this);
+
+  // Then, describe inputs needed by the module
+
+  // Add a new input
+  this->AddInputDescriptor<VectorImageType>("InputImage","The image to classify");
+  this->AddInputDescriptor<LabeledImageType>("LabeledImage","The segmentation of the image");
+}
+
+/** Destructor */
+ObjectLabelingModule::~ObjectLabelingModule()
+{}
+
+/** PrintSelf method */
+void ObjectLabelingModule::PrintSelf(std::ostream& os, itk::Indent indent) const
+{
+  // Call superclass implementation
+  Superclass::PrintSelf(os,indent);
+}
+
+
+/** The custom run command */
+void ObjectLabelingModule::Run()
+{
+  // Untill window closing, module will be busy
+  this->BusyOn();
+  
+   VectorImageType::Pointer fpvImage = this->GetInputData<VectorImageType>("InputImage");
+   LabeledImageType::Pointer lImage = this->GetInputData<LabeledImageType>("LabeledImage");
+
+   if(fpvImage.IsNotNull() && lImage.IsNotNull())
+     {
+     // Process the input as an FloatingVectorImageType
+     m_View->Build();
+     fpvImage->UpdateOutputInformation();
+     lImage->UpdateOutputInformation();
+     m_Model->OpenImage(fpvImage,lImage);
+     m_View->UpdateViewerSetup();
+     }
+   else
+     {
+       itkExceptionMacro(<<"One of the input image is null");
+     }
+}
+
+/** The Notify */
+void ObjectLabelingModule::Notify(const std::string & event)
+{
+
+   if (event == "OutputsUpdated")
+     {
+     this->ClearOutputDescriptors();
+     this->AddOutputDescriptor(m_Model->GetLabeledOutput(),"LabeledOutput","The classes labels image");
+     this->AddOutputDescriptor(m_Model->GetColoredOutput(),"ColoredOutput","The classes colors image");
+     this->NotifyOutputsChange();
+     }
+   else if(event == "Quit")
+     {
+     // Once module is closed, it is no longer busy
+     this->BusyOff();
+     }
+}
+} // End namespace otb
+
---
+++ Code/Modules/ObjectLabeling/otbObjectLabelingModule.h
@@ -0,0 +1,97 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __otbObjectLabelingModule_h
+#define __otbObjectLabelingModule_h
+
+// include the base class
+#include "otbModule.h"
+
+#include "otbObjectLabelingApplicationModel.h"
+#include "otbObjectLabelingApplicationView.h"
+#include "otbObjectLabelingApplicationController.h"
+#include "otbEventsListener.h"
+
+// include the OTB/ITK elements
+#include "otbVectorImage.h"
+#include "otbImage.h"
+
+namespace otb
+{
+/** \class ObjectLabelingModule
+ *
+ *  \brief This is the ObjectLabeling module, which allows to perform
+ *  ObjectLabeling filtering, segmentation and clustering.
+ *
+ *
+ */
+
+class ITK_EXPORT ObjectLabelingModule
+  : public Module, public EventsListener<std::string>
+{
+public:
+  /** Standard class typedefs */
+  typedef ObjectLabelingModule          Self;
+  typedef Module                        Superclass;
+  typedef itk::SmartPointer<Self>       Pointer;
+  typedef itk::SmartPointer<const Self> ConstPointer;
+
+  /** New macro */
+  itkNewMacro(Self);
+
+  /** Type macro */
+  itkTypeMacro(ObjectLabelingModule,Module);
+
+  /** MVC typedefs */
+  typedef otb::ObjectLabelingApplicationController ControllerType;
+  typedef otb::ObjectLabelingApplicationModel      ModelType;
+  typedef otb::ObjectLabelingApplicationView       ViewType;
+
+  /** Data types */
+  typedef ModelType::LabeledImageType         LabeledImageType;
+  typedef ModelType::VectorImageType          VectorImageType;
+
+protected:
+  /** Constructor */
+  ObjectLabelingModule();
+
+  /** Destructor */
+  virtual ~ObjectLabelingModule();
+
+  /** Notify Monteverdi application that featureExtraction has a result */
+  void Notify(const std::string & event);
+  
+  /** PrintSelf method */
+  virtual void PrintSelf(std::ostream& os, itk::Indent indent) const;
+
+  /** The custom run command */
+  virtual void Run();
+
+private:
+  ObjectLabelingModule(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+
+  ControllerType::Pointer m_Controller;
+  ViewType::Pointer m_View;
+  ModelType::Pointer m_Model;
+};
+
+
+} // End namespace otb
+
+#endif
