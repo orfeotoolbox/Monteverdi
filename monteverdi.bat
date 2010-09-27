5c0e6fef3fcb51fe413e41562f3fe968d73d7a2a
Julien Malik <julien.malik@orfeo-toolbox.org>
2010-09-27 15:14:08 +0200

    ENH: add a batch file to set up proper environment before launching monteverdi

---
+++ monteverdi.bat
@@ -0,0 +1,13 @@
+:: Monteverdi launcher to set up proper GDAL environment
+
+
+:: Get the directory of the current script 
+@set CURRENT_SCRIPT_DIR=%~dp0
+
+
+:: Set GDAL_DATA env. variable
+@set GDAL_DATA=%CURRENT_SCRIPT_DIR%..\share\gdal
+
+
+:: Start Monteverdi
+@%CURRENT_SCRIPT_DIR%monteverdi.exe
