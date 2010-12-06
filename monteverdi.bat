c92d09f3d7f64cfaf30d66cc394efea85c4dda99
Julien Malik <julien.malik@orfeo-toolbox.org>
2010-12-06 12:14:03 +0100

    BUG: launch monteverdi in TEMP dir on windows since it needs write access for curl

--- monteverdi.bat
+++ monteverdi.bat
@@ -1,13 +1,15 @@
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Monteverdi launcher to set up proper GDAL environment
-
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 :: Get the directory of the current script 
 @set CURRENT_SCRIPT_DIR=%~dp0
 
-
 :: Set GDAL_DATA env. variable
 @set GDAL_DATA=%CURRENT_SCRIPT_DIR%..\share\gdal
 
+:: Set current dir to TEMP dir because Monteverdi generates temporary files and need write access
+@cd %TEMP%
 
 :: Start Monteverdi
 @"%CURRENT_SCRIPT_DIR%monteverdi.exe"
