c5e249ed051acea44696e6d40c178a6261fe78f0
Mickael Savinaud <mickael.savinaud@c-s.fr>
2011-02-03 14:37:02 +0100

    ENH: modify the label in the case of hdf file

--- Code/Modules/Reader/otbReaderModule.cxx
+++ Code/Modules/Reader/otbReaderModule.cxx
@@ -142,7 +142,7 @@ void ReaderModule::Analyse()
 		  break;
 		}
 	  }
-	  catch (itk::ExceptionObject)
+	  catch (itk::ExceptionObject&)
 	  {
 	  // Silent catch
 	  }
@@ -157,7 +157,7 @@ void ReaderModule::Analyse()
 			  vType->value(3);
 			  typeFound = true;
 		  }
-		  catch (itk::ExceptionObject)
+		  catch (itk::ExceptionObject&)
 		  {
 			  // Silent catch
 			  vType->value(0);
