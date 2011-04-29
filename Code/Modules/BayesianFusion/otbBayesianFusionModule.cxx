9d045b1daa0cba1e1e3e3c075e1ab4e501421013
Julien Michel <julien.michel@orfeo-toolbox.org>
2011-04-29 17:03:17 +0200

    ENH: Adding a module to perform bayesian fusion (still missing a gui to set the lambda parameter)

---
+++ Code/Modules/BayesianFusion/otbBayesianFusionModule.cxx
@@ -0,0 +1,82 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "otbBayesianFusionModule.h"
+
+namespace otb
+{
+/** Constructor */
+BayesianFusionModule::BayesianFusionModule()
+{
+  // First, do constructor stuffs
+
+  // Then, describe inputs needed by the module
+
+  // Add a new input
+  this->AddInputDescriptor<FloatingImageType>("PanImage", otbGetTextMacro("Panchromatic image"));
+  this->AddInputDescriptor<FloatingVectorImageType>("ZoomXsImage", otbGetTextMacro("XS image superimposed to panchromatic image with cubic resampling"));
+  this->AddInputDescriptor<FloatingVectorImageType>("XsImage", otbGetTextMacro("XS image superimposed to panchromatic image with nearest neighbors resampling"));
+
+  m_BayesianFusionFilter = BayesianFusionFilterType::New();
+
+}
+
+/** Destructor */
+BayesianFusionModule::~BayesianFusionModule()
+{}
+
+/** PrintSelf method */
+void BayesianFusionModule::PrintSelf(std::ostream& os, itk::Indent indent) const
+{
+  // Call superclass implementation
+  Superclass::PrintSelf(os, indent);
+}
+
+/** The custom run command */
+void BayesianFusionModule::Run()
+{
+  // Here is the body of the module.
+  // When the Run() method is called, necessary inputs have been
+  // passed to the module.
+
+  // First step is to retrieve the inputs
+  FloatingImageType::Pointer       panImage = this->GetInputData<FloatingImageType>("PanImage");
+  FloatingVectorImageType::Pointer zoomXsImage = this->GetInputData<FloatingVectorImageType>("ZoomXsImage");
+  FloatingVectorImageType::Pointer xsImage = this->GetInputData<FloatingVectorImageType>("XsImage");
+
+
+  // Once all inputs have been properly retrieved, do what the module
+  // should do : show a gui, start an MVC model, trigger processing...
+
+  m_BayesianFusionFilter->SetPanchro(panImage);
+  m_BayesianFusionFilter->SetMultiSpect(xsImage);
+  m_BayesianFusionFilter->SetMultiSpectInterp(zoomXsImage);
+
+  // Then, when the module did actually produce some outputs, declare
+  // them.
+
+  // First, clear any previous output
+  this->ClearOutputDescriptors();
+
+  // Add an output (single version)
+  this->AddOutputDescriptor(m_BayesianFusionFilter->GetOutput(), "PanSharpenedImageOutput",
+                            otbGetTextMacro("Pansharpened image"));
+
+  // Last, when all outputs where declared, notify listeners
+  this->NotifyOutputsChange();
+}
+} // End namespace otb
---
+++ Code/Modules/BayesianFusion/otbBayesianFusionModule.h
@@ -0,0 +1,84 @@
+/*=========================================================================
+
+  Program:   ORFEO Toolbox
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) Centre National d'Etudes Spatiales. All rights reserved.
+  See OTBCopyright.txt for details.
+
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __otbBayesianFusionModule_h
+#define __otbBayesianFusionModule_h
+
+// include the base class
+#include "otbModule.h"
+
+// include the OTB/ITK elements
+#include "otbVectorImage.h"
+#include "otbImage.h"
+#include "otbBayesianFusionFilter.h"
+
+namespace otb
+{
+/** \class BayesianFusionModule
+ *  \brief This is the BayesianFusion module
+ *
+ * Description of the module.
+ *
+ */
+
+class ITK_EXPORT BayesianFusionModule
+  : public Module
+{
+public:
+  /** Standard class typedefs */
+  typedef BayesianFusionModule           Self;
+  typedef Module                        Superclass;
+  typedef itk::SmartPointer<Self>       Pointer;
+  typedef itk::SmartPointer<const Self> ConstPointer;
+
+  /** New macro */
+  itkNewMacro(Self);
+
+  /** Type macro */
+  itkTypeMacro(BayesianFusionModule, Module);
+
+  /** Data typedefs */
+  typedef TypeManager::Floating_Point_VectorImage FloatingVectorImageType;
+  typedef TypeManager::Floating_Point_Image       FloatingImageType;
+
+  typedef otb::BayesianFusionFilter<FloatingVectorImageType,FloatingVectorImageType,
+    FloatingImageType,FloatingVectorImageType> BayesianFusionFilterType;
+
+protected:
+  /** Constructor */
+  BayesianFusionModule();
+
+  /** Destructor */
+  virtual ~BayesianFusionModule();
+
+  /** PrintSelf method */
+  virtual void PrintSelf(std::ostream& os, itk::Indent indent) const;
+
+  /** The custom run command */
+  virtual void Run();
+
+private:
+  BayesianFusionModule(const Self&); //purposely not implemented
+  void operator =(const Self&); //purposely not implemented
+
+  BayesianFusionFilterType::Pointer m_BayesianFusionFilter;
+
+};
+
+} // End namespace otb
+
+#endif
