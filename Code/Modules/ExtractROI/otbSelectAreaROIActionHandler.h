76eb50855ffa9b98cd201467710c0d0a0ca3df5e
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2011-05-24 17:53:48 +0200

    BUG: fix roi selection bug in extractROI module #315

--- Code/Modules/ExtractROI/otbSelectAreaROIActionHandler.h
+++ Code/Modules/ExtractROI/otbSelectAreaROIActionHandler.h
@@ -73,12 +73,12 @@ public:
    * \param event The event
    * \return The handling return code
    */
-  virtual bool HandleWidgetEvent(std::string widgetId, int event)
+  virtual bool HandleWidgetEvent(const std::string & widgetId, int event)
   {
     // if left mouse button pressed
     if (Fl::event_button() != FL_LEFT_MOUSE)
       {
-      return false;
+        return false;
       }
 
     if (m_Model.IsNotNull() && this->GetIsActive())
