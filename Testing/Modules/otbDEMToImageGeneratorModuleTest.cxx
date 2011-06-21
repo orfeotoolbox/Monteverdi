a42d710b42817d4f85fd3c2dbe8c10fcf9008b5c
Cyrille Valladeau <cyrille.valladeau@c-s.fr>
2011-06-21 18:13:50 +0200

    ENH: improve test (reduce time) and style on the GUI)

--- Testing/Modules/otbDEMToImageGeneratorModuleTest.cxx
+++ Testing/Modules/otbDEMToImageGeneratorModuleTest.cxx
@@ -20,6 +20,7 @@
 #include "otbImage.h"
 #include "otbImageFileReader.h"
 #include "otbImageFileWriter.h"
+#include "otbExtractROI.h"
 
 int otbDEMToImageGeneratorModuleTest(int argc, char* argv[])
 {
@@ -29,36 +30,69 @@ int otbDEMToImageGeneratorModuleTest(int argc, char* argv[])
   std::cout << "Module: " << module << std::endl;
 
   // Put in the tests
-  const char * infname = argv[1];
-  const char * demrep  = argv[2];
+  const char * demrep  = argv[1];
 
   typedef otb::DEMToImageGeneratorModule::InputImageType  ImageType;
+  typedef ImageType::PixelType                            PixelType; 
   typedef otb::ImageFileReader<ImageType>                 ReaderType;
   typedef otb::ImageFileWriter<ImageType>                 WriterType;
+  typedef otb::ExtractROI<PixelType, PixelType>           ExtractType;
 
-  // reader
-  ReaderType::Pointer reader = ReaderType::New();
-  reader->SetFileName(infname);
-  reader->GenerateOutputInformation();
+  otb::DataObjectWrapper wrapperIn;
 
-  // Add Wrapper Input
-  otb::DataObjectWrapper wrapperIn = otb::DataObjectWrapper::Create(reader->GetOutput());
-  std::cout << "Input wrapper: " << wrapperIn << std::endl;
-  module->AddInputByKey("InputImage", wrapperIn);
+  if(argc == 4)
+    {
+      const char * infname = argv[2];
+      // reader
+      ReaderType::Pointer reader = ReaderType::New();
+      reader->SetFileName(infname);
+      reader->GenerateOutputInformation();
+      
+      ExtractType::Pointer extract = ExtractType::New();
+      extract->SetInput( reader->GetOutput() );
+      ImageType::RegionType region;
+      ImageType::SizeType   sz;
+      ImageType::IndexType  id;
 
-  module->Start();
+      sz.Fill(100);
+      id.Fill(100);
+      region.SetSize(sz);
+      region.SetIndex(id);
+      
+      extract->SetExtractionRegion(region);
+      extract->UpdateOutputInformation();
+      
+      // Add Wrapper Input
+      wrapperIn = otb::DataObjectWrapper::Create(extract->GetOutput());
+      std::cout << "Input wrapper: " << wrapperIn << std::endl;
+      module->AddInputByKey("InputImage", wrapperIn);
+      
+      module->Start();
+      
+    }
+  else if(argc == 9)
+    {
+      module->Start();
+      
+      specificModule->GetView()->oOriginX->value(argv[2]);
+      specificModule->GetView()->oOriginY->value(argv[3]);
+      specificModule->GetView()->oSizeX->value(argv[4]);
+      specificModule->GetView()->oSizeY->value(argv[5]);
+      specificModule->GetView()->oSpacingX->value(argv[6]);
+      specificModule->GetView()->oSpacingY->value(argv[7]);
 
+      Fl::check();
+    }
+ else
+    {
+      std::cout<<"Invalid input argument."<<std::endl;
+      return EXIT_FAILURE;
+    }
+  
   // Simulate the update of the output parameters :
   specificModule->GetView()->iDEMPath->value(demrep);
 
-  itk::OStringStream oss;
-  oss.str("500");
-  specificModule->GetView()->oSizeX->value(oss.str().c_str());
-  specificModule->GetView()->oSizeY->value(oss.str().c_str());
-  oss.str("50");
-  specificModule->GetView()->oSpacingX->value(oss.str().c_str()); ;
-  specificModule->GetView()->oSpacingY->value(oss.str().c_str()); ;
-//  specificModule->GetView()->oRadiusHillShading
+
   Fl::check();
 
   // Simulate Ok button callback
@@ -75,7 +109,7 @@ int otbDEMToImageGeneratorModuleTest(int argc, char* argv[])
 
   //Write the image
   WriterType::Pointer writer = WriterType::New();
-  writer->SetFileName(argv[3]);
+  writer->SetFileName(argv[argc-1]);
   writer->SetInput(outImage);
   writer->Update();
 
