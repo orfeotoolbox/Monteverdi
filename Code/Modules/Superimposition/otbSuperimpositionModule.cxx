06707311508720515ddd6015c2324e441ad1f636
Julien Malik <julien.malik@orfeo-toolbox.org>
2012-10-01 11:53:26 +0200

    WRG: remove unused variable

--- Code/Modules/Superimposition/otbSuperimpositionModule.cxx
+++ Code/Modules/Superimposition/otbSuperimpositionModule.cxx
@@ -129,11 +129,13 @@ void SuperimpositionModule::Browse()
 {
   const char * filename = NULL;
 
+  /*
   const char* defaultPath = "";
   if (otb::ConfigurationFile::GetInstance()->IsValid())
     {
     defaultPath = otb::ConfigurationFile::GetInstance()->GetDEMDirectory().c_str();
     }
+  */
   filename = flu_dir_chooser(otbGetTextMacro("Choose the DEM dir..."), "*.*", "");
 
   if (filename == NULL)
